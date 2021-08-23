5fca14af00a42ff71b9b4e5f16f7a16148aea589
ekulbyrnes <luke.e.byrnes@gmail.com>
2021-08-23 14:56:28 +1000

    display and iterate task list

--- django/tasks/views.py
+++ django/tasks/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 
 def index(request):
     tasks = Task.objects.all()
-
     
-    return render(request, 'tasks/list.html')
+    context = {'tasks':tasks}    
+    return render(request, 'tasks/list.html', context)
