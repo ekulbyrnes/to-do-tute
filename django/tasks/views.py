24ead267700853edd1719d477632119fd840b4d9
ekulbyrnes <luke.e.byrnes@gmail.com>
2021-08-23 23:04:24 +1000

    update delete functions. splash in BS5

--- django/tasks/views.py
+++ django/tasks/views.py
@@ -6,16 +6,44 @@ from .forms import *
 
 # Create your views here.
 
+# Create Tasks
 def index(request):
     tasks = Task.objects.all()
     
-    forms = TaskForm()
-
+    form = TaskForm()
+    
     if request.method =='POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/')
 
-    context = {'tasks':tasks, 'forms':forms}    
+    context = {'tasks':tasks, 'form':form}    
     return render(request, 'tasks/list.html', context)
+
+# Update tasks
+def update(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+
+    return render(request, 'tasks/update.html', context)
+
+# Delete task
+def delete(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item':item}
+    return render(request, 'tasks/delete.html', context)
