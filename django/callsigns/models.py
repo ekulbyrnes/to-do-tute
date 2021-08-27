74aad2ed4324e6dbf62415bcad765bc4a06c6488
ekulbyrnes <luke.e.byrnes@gmail.com>
2021-08-27 12:28:42 +1000

    removed OneDrive files

--- django/callsigns/models.py
+++ django/callsigns/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class contact(models.Model):
+    title= models.CharField(max_length=200)
+    
+
 class callsign(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
